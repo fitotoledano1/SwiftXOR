ddfed0a8ce997423d1dcc2631252e38a80abf7dd
Fito Toledano <fito.toledano@gmail.com>
2025-03-16 10:36:59 +0100

    Simplified XOR operator

--- Sources/SwiftXOR/SwiftXOR.swift
+++ Sources/SwiftXOR/SwiftXOR.swift
@@ -1,10 +1,12 @@
 extension Bool {
+    
+    /// XOR. A Boolean operation that returns true if, and only if, one of the two operands is true.
+    /// By definition, since `Bool` only has two possible values: `true`, and `false`;
+    /// if the left hand side operand is not equal to the right hand side, it means that:
+    /// - at least one of the two is `true` **and**
+    /// - the other one is `false`.
+    /// Therefore, we return true if lhs is not equal to rhs.
     static func ^ (lhs: Bool, rhs: Bool) -> Bool {
-        switch (lhs, rhs) {
-        case (true, true): return false
-        case (true, false): return true
-        case (false, true): return true
-        case (false, false): return false
-        }
+        lhs != rhs
     }
 }
